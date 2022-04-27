f7413afc20575fc2b696ccf5a48132aab1ef5151
seandowdall <78096782+seandowdall@users.noreply.github.com>
2022-04-27 14:19:54 +0100

    Add project files.

---
+++ software_assignment/InsuranceService.cs
@@ -0,0 +1,35 @@
+﻿namespace software_assignment
+{
+    public class InsuranceService
+    {
+        public float CalcPremium(int age, string location)
+        {
+            double premium;
+
+            if (location == “rural”)
+	        if ((age > = 18) && (age <= 30))
+                premium = 5.0;
+            else
+            if (age >= 31)
+                premium = 2.50;
+            else
+                premium = 0.0;
+            else
+            if (location == “urban”)
+	        if ((age > = 18) && (age <= 35))
+                premium = 6.0;
+            else
+           if (age >= 36)
+                premium = 5.0;
+            else
+                premium = 0.0;
+             else
+                premium = 0.0;
+
+            if (age >= 50)
+                premium = premium * 0.15;
+            return premium;
+        }
+
+    }
+}
---
+++ sut.tests/SutTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using software_assignment;
+
+namespace sut.tests
+{
+    [TestFixture]
+    class SutTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(24, "rural");
+            Assert.That(result, Is.EqualTo(5.0));
+        }
+        [Test]
+        public void test2()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(37, "rural");
+            Assert.That(result, Is.EqualTo(2.50));
+        }
+        [Test]
+        public void test3()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(12, "rural");
+            Assert.That(result, Is.EqualTo(0.0));
+        }
+        [Test]
+        public void test4()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(27, "urban");
+            Assert.That(result, Is.EqualTo(6.0));
+        }
+        [Test]
+        public void test5()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(40, "urban");
+            Assert.That(result, Is.EqualTo(5.0));
+        }
+        [Test]
+        public void test6()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(16, "urban");
+            Assert.That(result, Is.EqualTo(0.0));
+        }
+        [Test]
+        public void test7()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(67, "urban");
+            Assert.That(result, Is.EqualTo(0.75));
+        }
+        [Test]
+        public void test8()
+        {
+            InsuranceService ins = new InsuranceService();
+            double result = ins.CalcPremium(70, "rural");
+            Assert.That(result, Is.EqualTo(0.375));
+        }
+
+    }
+}
